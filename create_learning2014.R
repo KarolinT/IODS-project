07f00c3e29bd74490123432ead4a58e0a029c153
Karolin Toompere <a30869@ut.ee>
2020-11-06 15:59:09 +0200

    Data wrangling week 2 done!

--- create_learning2014.R
+++ create_learning2014.R
@@ -17,6 +17,7 @@
 #in the learning2014 data, 
   lrn14$attitude <- lrn14$Attitude/10
   
+  #scale all combination variables to original scales
   deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
   surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
   strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
@@ -31,14 +32,27 @@
   strategic_columns <- select(lrn14, one_of(strategic_questions))
   lrn14$stra <- rowMeans(strategic_columns)
   
+  #keep only necessary variables
   keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
-  # select rows where points is greater than zero
-  
   learning2014 <- select(lrn14, one_of(keep_columns))
-  colnames(learning2014)[2] <- "age"
-  # change the name of "Points" to "points"
-  colnames(learning2014)[7] <- "points"
   
+  #change names to lowercase
+  colnames(learning2014)[2] <- "age"
+   colnames(learning2014)[7] <- "points"
+ 
+  #keep only if points >0
   learning2014 <- filter(learning2014, points>0)
   
   dim(learning2014)
+  #166 obs, 7 var
+  
+#4  Save the analysis dataset to the ???dataÿ folder, 
+    #using for example write.csv() or write.table() functions.
+  
+  ?write.csv
+  write.table(learning2014, file = "learning2014.csv", row.names = TRUE, sep=";")
+  
+  #demonstrate that you can also read the data again
+  demonstrate=read.table("learning2014.csv", header= TRUE, sep=";")
+  
+  dim(demonstrate)
